7591e5bbc48840306b1d4655ed80fbaca69bddb6
Prelang Builder <builder@prelang.com>
2014-09-20 22:15:58 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/death_notices_controller.rb
+++ app/controllers/death_notices_controller.rb
@@ -3,7 +3,7 @@ class DeathNoticesController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_death_notice, only: [:show, :edit, :update, :destroy]
+  before_action :set_death_notice, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /death_notices
   # GET /death_notices.json
